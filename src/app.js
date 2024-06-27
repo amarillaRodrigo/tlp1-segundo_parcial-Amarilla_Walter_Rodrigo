61197c3c10bc8f5423b660e1eac999f679ea64d7
Rodrigo Amarilla <amarillarodrigo14@gmail.com>
2024-06-27 16:17:54 -0300

    CRUD de productos finalizado, examen finalizado

--- src/app.js
+++ src/app.js
@@ -5,6 +5,66 @@ import {database} from "./db.js"
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT, console.log('Server is on port:', PORT))
+app.use (express.json())
+
+app.listen(PORT, console.log('Servidor funcionando en el puerto:', PORT))
 
 
+// getAllProducts
+
+app.get("/products", (req, res) => {
+    res.json(database)
+})
+
+//obtener producto especifico por ID
+app.get("/products/:id", (req, res) => {
+    const productId = parseInt(req.params.id)
+    const findProduct = database.find((productos) => productos.id === productId)
+    res.json(findProduct)
+})
+
+
+// POST
+app.post("/products", (req, res) => {
+    const { id, name, quantity, price} = req.body
+    const newProduct = {
+        "id": id,
+        "name": name,
+        "quantity": quantity,
+        "price": price,
+    };
+    database.push(newProduct);
+    res.json(newProduct);
+    console.log("Producto Creado");
+});
+
+
+//Actualizar producto
+
+app.put("/products/:id", (req, res) => {
+    const productId = parseInt(req.params.id);
+    const { name, quantity, price } = req.body;
+
+    const findProduct = database.find((product) => product.id === productId);
+
+    if (findProduct) {
+        findProduct.name = name;
+        findProduct.quantity = quantity;
+        findProduct.price = price;
+
+        res.json(findProduct);
+        console.log("Producto Actualizado");
+    } else {
+        res.status(404).json({ message: "Producto no encontrado" });
+    }
+});
+
+
+//Borrar producto
+app.delete("/products/:id", (req, res) => {
+    const productId = parseInt(req.params.id)
+    const findProduct = database.find((productos) => productos.id === productId)
+    const indexProduct = database.indexOf(findProduct)
+    const deleteProduct = database.splice(indexProduct, 1)
+    console.log("Producto eliminado")
+})
--- src/db.js
+++ src/db.js
@@ -1,19 +1,19 @@
 export const database = [
     {
-        "id" : 1,
-        "name": 'Yerba',
+        "id": 1,
+        "name": "Yerba",
         "quantity": 5,
         "price": 9112.57,
     },
     {
-        "id" : 2,
-        "name": 'Azucar',
+        "id": 2,
+        "name": "Azucar",
         "quantity": 15,
         "price": 24580.90,
     },
     {
-        "id" : 3,
-        "name": 'Crema dental',
+        "id": 3,
+        "name": "Crema dental",
         "quantity": 3,
         "price": 7460.15,
     },
